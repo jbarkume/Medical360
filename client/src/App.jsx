78af8da56226a342805a8ce2c4f9f22fa8c052f5
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-03-25 22:53:56 -0400

    implement ui for landing page on startup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import RegistrationForm from "./components/RegistrationForm";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import HomePage from "./components/HomePage";
 function App() {
-
   return (
-    <div>
-      <RegistrationForm />
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/login" element={""} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
