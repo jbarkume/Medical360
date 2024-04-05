fa807756160c4e600d4341a86ed9a58a279eb1f5
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-04-05 12:06:20 -0400

    add page 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import EquipmentPage from "./pages/EquipmentPage";
 import RoomPage from "./pages/RoomPage";
 import PatientPage from "./pages/PatientPage";
 import ResourceManagementPage from "./pages/ResourceManagementPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 
 function App() {
   return (
@@ -50,6 +51,7 @@ function App() {
           <Route path="/room" element={<RoomPage />} />
           <Route path="/patientpage" element={<PatientPage />} />
           <Route path="/resource-management" element={<ResourceManagementPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
