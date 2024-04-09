7dee23a554d3008de00f2aca4c164809a2b5696d
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-04-08 20:16:22 -0400

    fix server

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'../client/dist/index.html'));
   });
 
 
