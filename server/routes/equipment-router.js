ebe3c2f398070053f1ab418ad0cb6a05bafa7468
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-04-14 15:54:12 -0400

    add comments to router files for more clarity

--- server/routes/equipment-router.js
+++ server/routes/equipment-router.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const EquipmentController = require('../controllers/equipment-controller');  // Adjust path as necessary
 
+
+// POST route to create a new room
 router.post('/', EquipmentController.createEquipment);
+
+// PUT to update equipment data by id
 router.put('/:id', EquipmentController.updateEquipment);
+
+// GET route to get all equipments
 router.get('/', EquipmentController.getAllEquipment);
+
+// GET route to get single equipment by id
 router.get('/:id', EquipmentController.getEquipment);
+
+// DELETE route to delete a specific equipment by ID
 router.delete('/:id', EquipmentController.deleteEquipment);
 
 module.exports = router;
--- server/routes/room-router.js
+++ server/routes/room-router.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const RoomController = require('../controllers/room-controller'); 
 
+// POST route to create a new room
 router.post('/', RoomController.createRoom);
+
+// PUT to update room data by id
 router.put('/:id', RoomController.updateRoom);
+
+// GET route to get all rooms
 router.get('/', RoomController.getAllRooms);
+
+// GET route to get single room by id
 router.get('/:id', RoomController.getRoom);
+
+// DELETE route to delete a specific room by ID
 router.delete('/:id', RoomController.deleteRoom);
 
 module.exports = router;
